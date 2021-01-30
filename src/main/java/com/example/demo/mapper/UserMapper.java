b5da27a8dbbd92a1189fae98f98f4339b3028039
itcast <hello@itcast.cn>
2021-01-30 13:07:37 +0800

    测试提交

---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+
+
+import com.example.demo.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+       void save(User user);
+}
---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public void save(User user) {
+        userMapper.save(user);
+    }
+}
